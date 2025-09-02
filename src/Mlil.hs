37ae47f77e70c560d5d7de4a1e799e892a74d730
logan leland <logan@leland.zip>
2025-09-02 17:48:53 -0400

    add sketch for lifting all possible mlil instructions

--- src/Mlil.hs
+++ src/Mlil.hs
@@ -428,6 +428,255 @@ create func exprIndex'  = do
              , exprIndex = exprIndex'
              }
   case mlOperation rawInst of
+    MLIL_NOP -> do
+       error $ ("Unimplemented: " ++ show "MLIL_NOP")
+    MLIL_SET_VAR -> do
+       error $ ("Unimplemented: " ++ show "MLIL_SET_VAR")
+    MLIL_SET_VAR_FIELD -> do
+       error $ ("Unimplemented: " ++ show "MLIL_SET_VAR_FIELD")
+    MLIL_SET_VAR_SPLIT -> do
+       error $ ("Unimplemented: " ++ show "MLIL_SET_VAR_SPLIT")
+    MLIL_ASSERT -> do
+       error $ ("Unimplemented: " ++ show "MLIL_ASSERT")
+    MLIL_FORCE_VER -> do
+       error $ ("Unimplemented: " ++ show "MLIL_FORCE_VER")
+    MLIL_LOAD -> do
+       error $ ("Unimplemented: " ++ show "MLIL_LOAD")
+    MLIL_LOAD_STRUCT -> do
+       error $ ("Unimplemented: " ++ show "MLIL_LOAD_STRUCT")
+    MLIL_STORE -> do
+       error $ ("Unimplemented: " ++ show "MLIL_STORE")
+    MLIL_STORE_STRUCT -> do
+       error $ ("Unimplemented: " ++ show "MLIL_STORE_STRUCT")
+    MLIL_VAR -> do
+       error $ ("Unimplemented: " ++ show "MLIL_VAR")
+    MLIL_VAR_FIELD -> do
+       error $ ("Unimplemented: " ++ show "MLIL_VAR_FIELD")
+    MLIL_VAR_SPLIT -> do
+       error $ ("Unimplemented: " ++ show "MLIL_VAR_SPLIT")
+    MLIL_ADDRESS_OF -> do
+       error $ ("Unimplemented: " ++ show "MLIL_ADDRESS_OF")
+    MLIL_ADDRESS_OF_FIELD -> do
+       error $ ("Unimplemented: " ++ show "MLIL_ADDRESS_OF_FIELD")
+    MLIL_CONST -> do
+       error $ ("Unimplemented: " ++ show "MLIL_CONST")
+    MLIL_CONST_DATA -> do
+       error $ ("Unimplemented: " ++ show "MLIL_CONST_DATA")
+    MLIL_CONST_PTR -> do
+      let rec = MediumLevelILConstPtrRec { constant = fromIntegral $ getOp rawInst 0 }
+      return $ MediumLevelILConstPtr rec
+    MLIL_EXTERN_PTR -> do
+       error $ ("Unimplemented: " ++ show "MLIL_EXTERN_PTR")
+    MLIL_FLOAT_CONST -> do
+       error $ ("Unimplemented: " ++ show "MLIL_FLOAT_CONST")
+    MLIL_IMPORT -> do
+       error $ ("Unimplemented: " ++ show "MLIL_IMPORT")
+    MLIL_ADD -> do
+       error $ ("Unimplemented: " ++ show "MLIL_ADD")
+    MLIL_ADC -> do
+       error $ ("Unimplemented: " ++ show "MLIL_ADC")
+    MLIL_SUB -> do
+       error $ ("Unimplemented: " ++ show "MLIL_SUB")
+    MLIL_SBB -> do
+       error $ ("Unimplemented: " ++ show "MLIL_SBB")
+    MLIL_AND -> do
+       error $ ("Unimplemented: " ++ show "MLIL_AND")
+    MLIL_OR -> do
+       error $ ("Unimplemented: " ++ show "MLIL_OR")
+    MLIL_XOR -> do
+       error $ ("Unimplemented: " ++ show "MLIL_XOR")
+    MLIL_LSL -> do
+       error $ ("Unimplemented: " ++ show "MLIL_LSL")
+    MLIL_LSR -> do
+       error $ ("Unimplemented: " ++ show "MLIL_LSR")
+    MLIL_ASR -> do
+       error $ ("Unimplemented: " ++ show "MLIL_ASR")
+    MLIL_ROL -> do
+       error $ ("Unimplemented: " ++ show "MLIL_ROL")
+    MLIL_RLC -> do
+       error $ ("Unimplemented: " ++ show "MLIL_RLC")
+    MLIL_ROR -> do
+       error $ ("Unimplemented: " ++ show "MLIL_ROR")
+    MLIL_RRC -> do
+       error $ ("Unimplemented: " ++ show "MLIL_RRC")
+    MLIL_MUL -> do
+       error $ ("Unimplemented: " ++ show "MLIL_MUL")
+    MLIL_MULU_DP -> do
+       error $ ("Unimplemented: " ++ show "MLIL_MULU_DP")
+    MLIL_MULS_DP -> do
+       error $ ("Unimplemented: " ++ show "MLIL_MULS_DP")
+    MLIL_DIVU -> do
+       error $ ("Unimplemented: " ++ show "MLIL_DIVU")
+    MLIL_DIVU_DP -> do
+       error $ ("Unimplemented: " ++ show "MLIL_DIVU_DP")
+    MLIL_DIVS -> do
+       error $ ("Unimplemented: " ++ show "MLIL_DIVS")
+    MLIL_DIVS_DP -> do
+       error $ ("Unimplemented: " ++ show "MLIL_DIVS_DP")
+    MLIL_MODU -> do
+       error $ ("Unimplemented: " ++ show "MLIL_MODU")
+    MLIL_MODU_DP -> do
+       error $ ("Unimplemented: " ++ show "MLIL_MODU_DP")
+    MLIL_MODS -> do
+       error $ ("Unimplemented: " ++ show "MLIL_MODS")
+    MLIL_MODS_DP -> do
+       error $ ("Unimplemented: " ++ show "MLIL_MODS_DP")
+    MLIL_NEG -> do
+       error $ ("Unimplemented: " ++ show "MLIL_NEG")
+    MLIL_NOT -> do
+       error $ ("Unimplemented: " ++ show "MLIL_NOT")
+    MLIL_SX -> do
+       error $ ("Unimplemented: " ++ show "MLIL_SX")
+    MLIL_ZX -> do
+       error $ ("Unimplemented: " ++ show "MLIL_ZX")
+    MLIL_LOW_PART -> do
+       error $ ("Unimplemented: " ++ show "MLIL_LOW_PART")
+    MLIL_JUMP -> do
+       error $ ("Unimplemented: " ++ show "MLIL_JUMP")
+    MLIL_JUMP_TO -> do
+       error $ ("Unimplemented: " ++ show "MLIL_JUMP_TO")
+    MLIL_RET_HINT -> do
+       error $ ("Unimplemented: " ++ show "MLIL_RET_HINT")
+    MLIL_CALL -> do
+       error $ ("Unimplemented: " ++ show "MLIL_CALL")
+    MLIL_CALL_UNTYPED -> do
+       error $ ("Unimplemented: " ++ show "MLIL_CALL_UNTYPED")
+    MLIL_CALL_OUTPUT -> do
+       error $ ("Unimplemented: " ++ show "MLIL_CALL_OUTPUT")
+    MLIL_CALL_PARAM -> do
+       error $ ("Unimplemented: " ++ show "MLIL_CALL_PARAM")
+    MLIL_SEPARATE_PARAM_LIST -> do
+       error $ ("Unimplemented: " ++ show "MLIL_SEPARATE_PARAM_LIST")
+    MLIL_SHARED_PARAM_SLOT -> do
+       error $ ("Unimplemented: " ++ show "MLIL_SHARED_PARAM_SLOT")
+    MLIL_RET -> do
+      src' <- getExprList func exprIndex' 0
+      let rec = MediumLevelILRetRec { src = src' }
+      return $ MediumLevelILRet rec
+    MLIL_NORET -> do
+       error $ ("Unimplemented: " ++ show "MLIL_NORET")
+    MLIL_IF -> do
+       error $ ("Unimplemented: " ++ show "MLIL_IF")
+    MLIL_GOTO -> do
+       error $ ("Unimplemented: " ++ show "MLIL_GOTO")
+    MLIL_CMP_E -> do
+       error $ ("Unimplemented: " ++ show "MLIL_CMP_E")
+    MLIL_CMP_NE -> do
+       error $ ("Unimplemented: " ++ show "MLIL_CMP_NE")
+    MLIL_CMP_SLT -> do
+       error $ ("Unimplemented: " ++ show "MLIL_CMP_SLT")
+    MLIL_CMP_ULT -> do
+       error $ ("Unimplemented: " ++ show "MLIL_CMP_ULT")
+    MLIL_CMP_SLE -> do
+       error $ ("Unimplemented: " ++ show "MLIL_CMP_SLE")
+    MLIL_CMP_ULE -> do
+       error $ ("Unimplemented: " ++ show "MLIL_CMP_ULE")
+    MLIL_CMP_SGE -> do
+       error $ ("Unimplemented: " ++ show "MLIL_CMP_SGE")
+    MLIL_CMP_UGE -> do
+       error $ ("Unimplemented: " ++ show "MLIL_CMP_UGE")
+    MLIL_CMP_SGT -> do
+       error $ ("Unimplemented: " ++ show "MLIL_CMP_SGT")
+    MLIL_CMP_UGT -> do
+       error $ ("Unimplemented: " ++ show "MLIL_CMP_UGT")
+    MLIL_TEST_BIT -> do
+       error $ ("Unimplemented: " ++ show "MLIL_TEST_BIT")
+    MLIL_BOOL_TO_INT -> do
+       error $ ("Unimplemented: " ++ show "MLIL_BOOL_TO_INT")
+    MLIL_ADD_OVERFLOW -> do
+       error $ ("Unimplemented: " ++ show "MLIL_ADD_OVERFLOW")
+    MLIL_SYSCALL -> do
+       error $ ("Unimplemented: " ++ show "MLIL_SYSCALL")
+    MLIL_SYSCALL_UNTYPED -> do
+       error $ ("Unimplemented: " ++ show "MLIL_SYSCALL_UNTYPED")
+    MLIL_TAILCALL -> do
+       error $ ("Unimplemented: " ++ show "MLIL_TAILCALL")
+    MLIL_TAILCALL_UNTYPED -> do
+       error $ ("Unimplemented: " ++ show "MLIL_TAILCALL_UNTYPED")
+    MLIL_INTRINSIC -> do
+       error $ ("Unimplemented: " ++ show "MLIL_INTRINSIC")
+    MLIL_FREE_VAR_SLOT -> do
+       error $ ("Unimplemented: " ++ show "MLIL_FREE_VAR_SLOT")
+    MLIL_BP -> do
+       error $ ("Unimplemented: " ++ show "MLIL_BP")
+    MLIL_TRAP -> do
+       error $ ("Unimplemented: " ++ show "MLIL_TRAP")
+    MLIL_UNDEF -> do
+       error $ ("Unimplemented: " ++ show "MLIL_UNDEF")
+    MLIL_UNIMPL -> do
+       error $ ("Unimplemented: " ++ show "MLIL_UNIMPL")
+    MLIL_UNIMPL_MEM -> do
+       error $ ("Unimplemented: " ++ show "MLIL_UNIMPL_MEM")
+    MLIL_FADD -> do
+       error $ ("Unimplemented: " ++ show "MLIL_FADD")
+    MLIL_FSUB -> do
+       error $ ("Unimplemented: " ++ show "MLIL_FSUB")
+    MLIL_FMUL -> do
+       error $ ("Unimplemented: " ++ show "MLIL_FMUL")
+    MLIL_FDIV -> do
+       error $ ("Unimplemented: " ++ show "MLIL_FDIV")
+    MLIL_FSQRT -> do
+       error $ ("Unimplemented: " ++ show "MLIL_FSQRT")
+    MLIL_FNEG -> do
+       error $ ("Unimplemented: " ++ show "MLIL_FNEG")
+    MLIL_FABS -> do
+       error $ ("Unimplemented: " ++ show "MLIL_FABS")
+    MLIL_FLOAT_TO_INT -> do
+       error $ ("Unimplemented: " ++ show "MLIL_FLOAT_TO_INT")
+    MLIL_INT_TO_FLOAT -> do
+       error $ ("Unimplemented: " ++ show "MLIL_INT_TO_FLOAT")
+    MLIL_FLOAT_CONV -> do
+       error $ ("Unimplemented: " ++ show "MLIL_FLOAT_CONV")
+    MLIL_ROUND_TO_INT -> do
+       error $ ("Unimplemented: " ++ show "MLIL_ROUND_TO_INT")
+    MLIL_FLOOR -> do
+       error $ ("Unimplemented: " ++ show "MLIL_FLOOR")
+    MLIL_CEIL -> do
+       error $ ("Unimplemented: " ++ show "MLIL_CEIL")
+    MLIL_FTRUNC -> do
+       error $ ("Unimplemented: " ++ show "MLIL_FTRUNC")
+    MLIL_FCMP_E -> do
+       error $ ("Unimplemented: " ++ show "MLIL_FCMP_E")
+    MLIL_FCMP_NE -> do
+       error $ ("Unimplemented: " ++ show "MLIL_FCMP_NE")
+    MLIL_FCMP_LT -> do
+       error $ ("Unimplemented: " ++ show "MLIL_FCMP_LT")
+    MLIL_FCMP_LE -> do
+       error $ ("Unimplemented: " ++ show "MLIL_FCMP_LE")
+    MLIL_FCMP_GE -> do
+       error $ ("Unimplemented: " ++ show "MLIL_FCMP_GE")
+    MLIL_FCMP_GT -> do
+       error $ ("Unimplemented: " ++ show "MLIL_FCMP_GT")
+    MLIL_FCMP_O -> do
+       error $ ("Unimplemented: " ++ show "MLIL_FCMP_O")
+    MLIL_FCMP_UO -> do
+       error $ ("Unimplemented: " ++ show "MLIL_FCMP_UO")
+    MLIL_SET_VAR_SSA -> do
+       error $ ("Unimplemented: " ++ show "MLIL_SET_VAR_SSA")
+    MLIL_SET_VAR_SSA_FIELD -> do
+       error $ ("Unimplemented: " ++ show "MLIL_SET_VAR_SSA_FIELD")
+    MLIL_SET_VAR_SPLIT_SSA -> do
+       error $ ("Unimplemented: " ++ show "MLIL_SET_VAR_SPLIT_SSA")
+    MLIL_SET_VAR_ALIASED -> do
+       error $ ("Unimplemented: " ++ show "MLIL_SET_VAR_ALIASED")
+    MLIL_SET_VAR_ALIASED_FIELD -> do
+       error $ ("Unimplemented: " ++ show "MLIL_SET_VAR_ALIASED_FIELD")
+    MLIL_VAR_SSA -> do
+      src <- getSSAVar rawInst 0 1
+      let rec = MediumLevelILVarSsaRec { src = src, var = src }
+      return $ MediumLevelILVarSsa rec
+    MLIL_VAR_SSA_FIELD -> do
+       error $ ("Unimplemented: " ++ show "MLIL_VAR_SSA_FIELD")
+    MLIL_VAR_ALIASED -> do
+       error $ ("Unimplemented: " ++ show "MLIL_VAR_ALIASED")
+    MLIL_VAR_ALIASED_FIELD -> do
+       error $ ("Unimplemented: " ++ show "MLIL_VAR_ALIASED_FIELD")
+    MLIL_VAR_SPLIT_SSA -> do
+       error $ ("Unimplemented: " ++ show "MLIL_VAR_SPLIT_SSA")
+    MLIL_ASSERT_SSA -> do
+       error $ ("Unimplemented: " ++ show "MLIL_ASSERT_SSA")
+    MLIL_FORCE_VER_SSA -> do
+       error $ ("Unimplemented: " ++ show "MLIL_FORCE_VER_SSA")
     MLIL_CALL_SSA -> do
       outputInst <- getExpr func $ getOp rawInst 0
       output <- case outputInst of
@@ -446,7 +695,18 @@ create func exprIndex'  = do
              , core = coreInst
              }
       return $ MediumLevelILCallSsa rec
-
+    MLIL_CALL_UNTYPED_SSA -> do
+       error $ ("Unimplemented: " ++ show "MLIL_CALL_UNTYPED_SSA")
+    MLIL_SYSCALL_SSA -> do
+       error $ ("Unimplemented: " ++ show "MLIL_SYSCALL_SSA")
+    MLIL_SYSCALL_UNTYPED_SSA -> do
+       error $ ("Unimplemented: " ++ show "MLIL_SYSCALL_UNTYPED_SSA")
+    MLIL_TAILCALL_SSA -> do
+       error $ ("Unimplemented: " ++ show "MLIL_TAILCALL_SSA")
+    MLIL_TAILCALL_UNTYPED_SSA -> do
+       error $ ("Unimplemented: " ++ show "MLIL_TAILCALL_UNTYPED_SSA")
+    MLIL_CALL_PARAM_SSA -> do
+       error $ ("Unimplemented: " ++ show "MLIL_CALL_PARAM_SSA")
     MLIL_CALL_OUTPUT_SSA -> do
       destMemory <- getInt rawInst 0
       dest <- getSSAVarList func 1 2
@@ -456,20 +716,25 @@ create func exprIndex'  = do
              , core = coreInst
              }
       return $ MediumLevelILCallOutputSsa rec
-
-    MLIL_CONST_PTR -> do
-      let rec = MediumLevelILConstPtrRec { constant = fromIntegral $ getOp rawInst 0 }
-      return $ MediumLevelILConstPtr rec
-
-    MLIL_RET -> do
-      src' <- getExprList func exprIndex' 0
-      let rec = MediumLevelILRetRec { src = src' }
-      return $ MediumLevelILRet rec
-
-    MLIL_VAR_SSA -> do
-      src <- getSSAVar rawInst 0 1
-      let rec = MediumLevelILVarSsaRec { src = src, var = src }
-      return $ MediumLevelILVarSsa rec
-
-    _ -> error $ ("Unimplemented: " ++ show coreInst)
+    MLIL_MEMORY_INTRINSIC_OUTPUT_SSA -> do
+       error $ ("Unimplemented: " ++ show "MLIL_MEMORY_INTRINSIC_OUTPUT_SSA")
+    MLIL_LOAD_SSA -> do
+       error $ ("Unimplemented: " ++ show "MLIL_LOAD_SSA")
+    MLIL_LOAD_STRUCT_SSA -> do
+       error $ ("Unimplemented: " ++ show "MLIL_LOAD_STRUCT_SSA")
+    MLIL_STORE_SSA -> do
+       error $ ("Unimplemented: " ++ show "MLIL_STORE_SSA")
+    MLIL_STORE_STRUCT_SSA -> do
+       error $ ("Unimplemented: " ++ show "MLIL_STORE_STRUCT_SSA")
+    MLIL_INTRINSIC_SSA -> do
+       error $ ("Unimplemented: " ++ show "MLIL_INTRINSIC_SSA")
+    MLIL_MEMORY_INTRINSIC_SSA -> do
+       error $ ("Unimplemented: " ++ show "MLIL_MEMORY_INTRINSIC_SSA")
+    MLIL_FREE_VAR_SLOT_SSA -> do
+       error $ ("Unimplemented: " ++ show "MLIL_FREE_VAR_SLOT_SSA")
+    MLIL_VAR_PHI -> do
+       error $ ("Unimplemented: " ++ show "MLIL_VAR_PHI")
+    MLIL_MEM_PHI -> do
+       error $ ("Unimplemented: " ++ show "MLIL_MEM_PHI")
+    _ -> error $ ("Unknown instruction type: " ++ show rawInst)
 
